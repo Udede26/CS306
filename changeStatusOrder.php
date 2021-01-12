321cac768da58a0c5748612f9468424ee8fe86df
berkeodaci <66134122+berkeodaci@users.noreply.github.com>
2021-01-13 00:25:36 +0300

    Add files via upload

--- changeStatusOrder.php
+++ changeStatusOrder.php
@@ -102,9 +102,6 @@ $_SESSION['orderid'] = $_REQUEST['orderid'];
         <a href="salesManager.php" class="navbar-brand d-flex align-items-center">
           <strong>Orders</strong>
         </a>
-        <a href="invoices.php" class="navbar-brand d-flex align-items-center">
-          <strong>Invoices</strong>
-        </a>
         </div>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
