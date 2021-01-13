0f9d0bc567cf6707cd3701ca61cebf985566ce8d
ecemsamlioglu <77124857+ecemsamlioglu@users.noreply.github.com>
2021-01-13 18:31:20 +0300

    Update products.php

--- products.php
+++ products.php
@@ -81,25 +81,31 @@
           <img class="mb-4" src="https://drive.google.com/uc?export=view&id=1MbY3FN3HvBnFjl3HQROjgaXkBq5nhq_V" id="cart" lt="" width="72" height="57">
 
           <div class="dropdown-content" id="mydropdown">
+     <?php
 
-            <div id="cartheader">
-              <a id="total"> Total:</a>
-              <button id="proceed" float:right> Proceed to Checkout</button>
-            </div>
+            $user_id = $_SESSION['users_id'];
 
-            <?php
             $db = mysqli_connect('localhost', 'root', '', 'step4');
             if ($db->connect_errno > 0) {
               die('Baglanamadim [' . $db->connect_error . ']');
             }
 
-            $result = mysqli_query($db, "SELECT * FROM product");
+            $result = mysqli_query($db, "SELECT* FROM BasketProducts BP, Product P, Basket B WHERE BP.user_id=$user_id AND P.product_id=BP.product_id AND B.user_id=BP.user_id");
 
+          if(mysqli_num_rows($result)>0)
+          {
             while ($row = mysqli_fetch_assoc($result)) {
               $product_name = $row['product_name'];
               $description = $row['product_description'];
               $price = $row['price'];
               $brand = $row['brand'];
+              $count_sag_ust = $row['countt'];
+              $total_sag_ust =$row['total_cost'];
+              
+              echo"<div id='cartheader'>";
+              echo"<a id='total'> Total: $$total_sag_ust </a>";
+              echo"<a href='checkout.php'><button id='proceed' float:right> Proceed to Checkout</button></a>";
+             echo"</div>";
 
               echo "<li class='list-group-item'>";
               echo "<!-- Custom content-->";
@@ -109,11 +115,26 @@
               echo      "<h5 class='mt-0 font-weight-bold mb-2'>$product_name</h5>";
               echo       "<p class='font-italic text-muted mb-0 small'>$description</p>";
               echo "<div class='mt-0 font-weight-bold mb-2'>
-                <h6 class='font-weight-bold my-2'>$price $</h6>
+                <h6 class='font-weight-bold my-2'>$$price x $count_sag_ust </h6>
                 
                 
                   </div>";
             }
+          }
+          else
+          {
+             
+             echo"<div id='cartheader'>";
+              echo"<a id='total'> Total: $0 </a>";
+              echo"<a href='checkout.php'><button id='proceed' float:right> Proceed to Checkout</button></a>";
+             echo"</div>";
+
+              echo "<li class='list-group-item'>";
+              echo "<!-- Custom content-->";
+              echo "<div class='media align-items-lg-center flex-column flex-lg-row p-3'>";
+              echo   "<div class='media-body order-2 order-sm-1'>";
+              echo "There is no product in the cart";
+          }
             ?>
           </div>
         </div>
