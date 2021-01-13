4a911f5b5d7f75f626f8a3f593d86fde97219fb3
ecemsamlioglu <77124857+ecemsamlioglu@users.noreply.github.com>
2021-01-13 15:01:07 +0300

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 include "config.php";
 
 if(!empty($_POST['sign_in_email']) && !empty($_POST['sign_in_password']))
@@ -8,7 +8,7 @@ if(!empty($_POST['sign_in_email']) && !empty($_POST['sign_in_password']))
    $user_signin_password =$_POST['sign_in_password'];
 
 
-  $sql4= "SELECT U.first_name, U.last_name, R.email, R.password
+  $sql4= "SELECT U.first_name, U.last_name, U.user_id, R.email, R.password
           FROM Users U, registereduserinfo R
           WHERE U.user_id = R.user_id ";
   $result = mysqli_query($db, $sql4);
@@ -18,8 +18,9 @@ if(!empty($_POST['sign_in_email']) && !empty($_POST['sign_in_password']))
       if($user_signin_email==$row['email'] && $user_signin_password==$row['password'])
       {
          //storing the name and surname for further use
-         $user_signin_name = $row['first_name'];
-         $user_signin_surname = $row['last_name'];
+         $_SESSION['user_signin_name']  = $row['first_name'];
+         $_SESSION['user_signin_surname']  = $row['last_name'];
+         $_SESSION['users_id']  = $row['user_id'];
 
          header('Location: home.php');
          exit;
@@ -39,3 +40,4 @@ else
    //echo("<script>window.location = 'index.php';</script>");
    echo "'Please fill in all the areas!"."<br>"."Redirecting to login page...";
 }
+?>
