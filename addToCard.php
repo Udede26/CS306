11ad2929d264ff3b5980bc01a5c73e35128f3352
berkeodaci <66134122+berkeodaci@users.noreply.github.com>
2021-01-13 19:21:28 +0300

    Add files via upload

---
+++ addToCard.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+//if(isset($_POST['']))
+include "config.php";
+$user_id = $_SESSION['users_id'];
+$pID =$_POST['product_id'];
+$pCount = $_POST['countt']
+
+
+$sql1= "SELECT P.price 
+        FROM product P
+        WHERE P.product_id = $pID";
+$result = mysqli_query($db, $sql1);
+while($row = mysqli_fetch_assoc($result))
+{
+  $price = $row['price'];
+}
+ 
+
+$sql2="SELECT * 
+        FROM basket B
+        WHERE B.user_id = $user_id";
+
+$result = mysqli_query($db, $sql2);
+while($row = mysqli_fetch_assoc($result))
+{
+  $basketid = $row['basketid'];
+  $totalCost = $row['total_cost'];
+  $numOfProducts = $row['num_of_products'];
+}
+
+$numOfProducts += $pCount;
+
+$sql_update= "UPDATE basket SET num_of_products = '$numOfProducts' WHERE basket_id = $basketid";
+mysqli_query($db, $sql_update);
+
+$totalCost += ($pCount * $price);
+
+$sql_update= "UPDATE basket SET total_cost = '$totalCost' WHERE basket_id = $basketid";
+mysqli_query($db, $sql_update);
+
+
+header('Location: products.php');
+exit;
+
+?>
