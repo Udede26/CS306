975be87424ef23806deae0a409245f4c6addffc4
berkeodaci <66134122+berkeodaci@users.noreply.github.com>
2021-01-13 22:38:35 +0300

    Add files via upload

--- addToCard.php
+++ addToCard.php
@@ -6,6 +6,20 @@ $user_id = $_SESSION['users_id'];
 $pID =$_POST['product_id'];
 $pCount = $_POST['countt'];
 
+$sql4= "SELECT *
+          FROM basketproducts B";
+$result = mysqli_query($db, $sql4);
+
+$found = false;
+while($row = mysqli_fetch_assoc($result))
+{
+    
+  if($user_id==$row['user_id'] && $pID == $row['product_id'])
+  {
+    $found = true;
+  }
+}
+
 
 $sql1= "SELECT P.price 
         FROM product P
@@ -39,13 +53,30 @@ $totalCost += ($pCount * $price);
 $sql_update= "UPDATE basket SET total_cost = '$totalCost' WHERE basket_id = $basketid";
 mysqli_query($db, $sql_update);
 
-
-$sql_statement= "INSERT INTO basketproducts(basket_id, user_id, product_id, countt)
+if($found)
+{
+	$sql1= "SELECT B.countt 
+        	FROM basketproducts B
+        	WHERE B.product_id = $pID AND B.basket_id = $basketid";
+	$result = mysqli_query($db, $sql1);
+	while($row = mysqli_fetch_assoc($result))
+	{
+  		$count = $row['countt'];
+	}
+	$pCount += $count;
+	$sql_update= "UPDATE basketproducts SET countt = $pCount WHERE product_id = $pID AND user_id = $user_id";
+	mysqli_query($db, $sql_update);
+}
+else
+{
+	$sql_statement= "INSERT INTO basketproducts(basket_id, user_id, product_id, countt)
                   VALUES ($basketid, $user_id, $pID, $pCount)";
-mysqli_query($db, $sql_statement);
+
+	mysqli_query($db, $sql_statement);
+}
+
 
 
 header('Location:products.php');
 exit;
-
 ?>
