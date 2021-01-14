47c1f1dbc945c88daafe2e14e1789cbfdd6fb1c1
ecemsamlioglu <77124857+ecemsamlioglu@users.noreply.github.com>
2021-01-14 23:00:18 +0300

    insert into order and invoice yapıldı

--- paymentOps.php
+++ paymentOps.php
@@ -6,18 +6,14 @@ include "config.php";
 $var1 = $_SESSION['users_id'];
 
 
-//$manager_query ="SELECT S.manager_id
-//FROM salesmanager S";
-//$result_man = mysqli_query($db, $manager_query);
-//$stop = rand(1,10);
+$manager_query ="SELECT COUNT(S.manager_id) AS COUNT FROM salesmanager S";
+$result_man = mysqli_query($db, $manager_query);
+
 //$start = 0;
-//while ($row = mysqli_fetch_assoc($result_man)) {
- //  if ($start == $stop)
-//   {
-//     $manager = $row['manager_id'];
-//   }
-//   $start++;
-//}
+while ($row = mysqli_fetch_assoc($result_man)) {
+
+    $manager = rand(1, $row['COUNT']);
+}
 
 $basket_query ="SELECT B.basket_id, B.total_cost
 FROM Basket B 
@@ -38,23 +34,18 @@ while ($row = mysqli_fetch_assoc($result_basket)) {
 
 
 
-$time_now = date('Y-m-d');
-echo $time_now;
+$sql_order= "INSERT INTO Place_Order(user_id, manager_id, basket_id, order_date, order_status)
+VALUES ($var1, $manager, $basket, CURDATE() , 'ordered')";
 
-
-
-$sql_order= "INSERT INTO Place_Order(order_id, user_id, manager_id, basket_id, order_date, order_status)
-VALUES ('$var1', 7, '$basket', '$time_now' , 'ordered')";
-
-$insert_order = mysqli_query($db, $sql_order);
+mysqli_query($db, $sql_order);
 
 $latest_order_id = mysqli_insert_id($db);
 
-$sql_invoice = "INSERT INTO Invoice(invoice_id, invoice_date, invoice_address, invoice_sum, order_id, user_id)
-VALUES ($time_now,  $var_invoice_address, $sum,  $latest_order_id, $var1)";
-$insert_invoice = mysqli_query($db, $sql_invoice);
 
+$sql_invoice = "INSERT INTO Invoice(invoice_date, invoice_address, invoice_sum, order_id, user_id)
+VALUES (CURDATE(),  '$var_invoice_address', $sum,  $latest_order_id, $var1)";
+mysqli_query($db, $sql_invoice);
 
- header('Location: home.php');
+header('Location: home_after_payment.php');
 
 ?>
