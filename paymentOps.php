6af43b78525bc647e5b4b134e935287c461437e7
ecemsamlioglu <77124857+ecemsamlioglu@users.noreply.github.com>
2021-01-20 06:29:59 +0300

    insert into place_order, invoice, orderedbasket

--- paymentOps.php
+++ paymentOps.php
@@ -41,6 +41,25 @@ mysqli_query($db, $sql_order);
 
 $latest_order_id = mysqli_insert_id($db);
 
+$basketproducts_query ="SELECT *
+FROM BasketProducts B, Product P
+WHERE B.basket_id=$basket AND B.product_id = P.product_id";
+$result_basketproducts = mysqli_query($db, $basketproducts_query);
+while ($row10 = mysqli_fetch_assoc($result_basketproducts)) {
+     
+     $product_id_basket = $row10['product_id'];
+     $counttt = $row10['countt'];
+     $total_price = $row10['price']*$counttt;
+
+     $sql_orderedbasket = "INSERT INTO OrderedBasketProducts(basket_id, user_id, order_id, product_id, total_price, counttt)
+VALUES($basket, $var1, $latest_order_id, $product_id_basket, $total_price, $counttt)";
+mysqli_query($db, $sql_orderedbasket);
+   
+}
+
+
+
+
 
 $sql_invoice = "INSERT INTO Invoice(invoice_date, invoice_address, invoice_sum, order_id, user_id)
 VALUES (CURDATE(),  '$var_invoice_address', $sum,  $latest_order_id, $var1)";
