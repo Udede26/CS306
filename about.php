b96bb64ef73531d486d9d617cdc4ecffb691fc67
ecemsamlioglu <77124857+ecemsamlioglu@users.noreply.github.com>
2021-01-20 21:41:09 +0300

    cart resimleri düzeltildi

--- about.php
+++ about.php
@@ -90,13 +90,15 @@
             }
 
             $result = mysqli_query($db, "SELECT* FROM BasketProducts BP, Product P, Basket B WHERE BP.user_id=$user_id AND P.product_id=BP.product_id AND B.user_id=BP.user_id");
-
+          
           if(mysqli_num_rows($result)>0)
           {
-            echo"<div id='cartheader'>";
+            
+            echo"<div id='cartheader'>";         
             echo"<a href='checkout.php'><button id='proceed' float:right> Proceed to Checkout</button></a>";
            echo"</div>";
-
+           echo"<br>";
+              echo"<br>";
             while ($row = mysqli_fetch_assoc($result)) {
               $product_name = $row['product_name'];
               $description = $row['product_description'];
@@ -105,30 +107,33 @@
               $count_sag_ust = $row['countt'];
               $total_sag_ust =$row['total_cost'];
               $product_id =$row['product_id'];
+              $product_picture = $row['product_picture'];
              
+              
               echo "<li class='list-group-item'>";
               echo "<!-- Custom content-->";
               echo "<div class='media align-items-lg-center flex-column flex-lg-row p-3'>";
               echo   "<div class='media-body order-2 order-sm-1'>";
-              echo      "</div><img src='https://drive.google.com/uc?export=view&id=1MbY3FN3HvBnFjl3HQROjgaXkBq5nhq_V' alt='Generic placeholder image' width='100' class='ml-lg-5 order-1 order-lg-2'>";
+              echo      "</div><img src=$product_picture alt='Generic placeholder image' width='100' class='ml-lg-5 order-1 order-lg-2'>";
               echo      "<h5 class='mt-0 font-weight-bold mb-2'>$product_name</h5>";
               echo       "<p class='font-italic text-muted mb-0 small'>$description</p>";
               echo "<div class='mt-0 font-weight-bold mb-2'>
-                <h6 class='font-weight-bold my-2'>$$price x $count_sag_ust </h6>
-                
-                
-                  </div>";
-                  echo"<form action='deleteFromCard.php' method='POST'>
+                <h6 class='font-weight-bold my-2'>$$price x $count_sag_ust </h6><form action='deleteFromCard.php' method='POST'>
                 
                 
                  
-                  <input style='width: 50px' value = 1 class='form-control my-2' name='countt' type='text' placeholder='countt' aria-label='Amount'>     
-                  <button type='submit' class='btn btn-sm btn-outline-secondary'>Add</button> 
-                  </form>
-                  <form action='deleteFromCard.php' method='POST'>
-                  <button type='submit' class='btn btn-sm btn-outline-secondary' name='product_id' value=$product_id>Delete</button>  
-                  </form>";
+                <input style='width: 50px' value = 1 class='form-control my-2' name='countt' type='text' placeholder='countt' aria-label='Amount'>     
+                <button type='submit' class='btn btn-sm btn-outline-secondary'>Add</button> 
+                </form>
+                <form action='deleteFromCard.php' method='POST'>
+                <button type='submit' class='btn btn-sm btn-outline-secondary'name='product_id' value=$product_id>Delete</button>  
+                </form>
+                ";
+                
+                
+                 echo" </div></li>";
             }
+            echo " "."Total: $$total_sag_ust";
           }
           else
           {
